e7be72f23994f0cdcdccc1c6f36fff1558a7aa6e
yuki sawabe <sawabe0v0@gmail.com>
2022-12-07 17:11:05 +0900

    create api to create user

--- index.ts
+++ index.ts
@@ -1,14 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 const prisma = new PrismaClient();
 
 app.get("/users", async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
   return res.json(users);
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    return res.json(user);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
