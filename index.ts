66508f401ed5596605cfaa4c4a6a828818a3410b
yuki sawabe <sawabe0v0@gmail.com>
2022-12-08 07:00:51 +0900

    create api to delete specified post

--- index.ts
+++ index.ts
@@ -128,4 +128,18 @@ app.patch("/posts/:id", async (req: Request, res: Response) => {
   }
 });
 
+app.delete("/posts/:id", async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  try {
+    const post = await prisma.post.delete({
+      where: {
+        id,
+      },
+    });
+    return res.json(post);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
