806c623901b4201c8966b8de51c78d32adb0b6c6
yuki sawabe <sawabe0v0@gmail.com>
2022-12-12 18:29:35 +0900

    modify seed.ts file

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-async function main() {
+const main = async (): Promise<void> => {
   const alice = await prisma.user.upsert({
     where: { email: "alice@prisma.io" },
     update: {},
@@ -12,13 +12,14 @@ async function main() {
   });
 
   console.log({ alice });
-}
+};
 
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
+  .then(async () => {
     await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
   });
