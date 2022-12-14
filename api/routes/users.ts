a3551099a764e2dc984d964ba57c29b85f023fac
yuki sawabe <sawabe0v0@gmail.com>
2022-12-14 17:46:44 +0900

    update routes/users.ts

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -1,75 +1,13 @@
-import { PrismaClient } from "@prisma/client";
-import { Request, Response } from "express";
+import express from "express";
 
-const prisma = new PrismaClient();
-
-const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  return res.json(users);
-});
+const UserController = require("../controllers/UserController");
 
-router.get("/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        id,
-      },
-    });
-    return res.json(user);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
-
-router.post("/", async (req: Request, res: Response) => {
-  const { name, email } = req.body;
-  try {
-    const user = await prisma.user.create({
-      data: {
-        name,
-        email,
-      },
-    });
-    return res.json(user);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
-
-router.patch("/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const { name } = req.body;
-  try {
-    const user = await prisma.user.update({
-      where: {
-        id,
-      },
-      data: {
-        name,
-      },
-    });
-    return res.json(user);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
-
-router.delete("/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  try {
-    const user = await prisma.user.delete({
-      where: {
-        id,
-      },
-    });
-    return res.json(user);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
+router.get("/", UserController.index);
+router.get("/:id", UserController.show);
+router.post("/", UserController.create);
+router.patch("/:id", UserController.update);
+router.delete("/:id", UserController.delete);
 
 module.exports = router;
