789d61bbe586d7c268ac8b03df56c3ae8272ceda
yuki sawabe <sawabe0v0@gmail.com>
2022-12-14 17:46:52 +0900

    update routes/posts.ts

--- api/routes/posts.ts
+++ api/routes/posts.ts
@@ -1,77 +1,13 @@
-import { PrismaClient } from "@prisma/client";
-import { Request, Response } from "express";
+import express from "express";
 
-const prisma = new PrismaClient();
-
-const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  const posts = await prisma.post.findMany();
-  return res.json(posts);
-});
+const PostController = require("../controllers/PostController");
 
-router.post("/", async (req: Request, res: Response) => {
-  const { title, content, authorId } = req.body;
-  try {
-    const post = await prisma.post.create({
-      data: {
-        title,
-        content,
-        authorId,
-      },
-    });
-    return res.json(post);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
-
-router.patch("/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const { title, content } = req.body;
-  try {
-    const post = await prisma.post.update({
-      where: {
-        id,
-      },
-      data: {
-        title,
-        content,
-      },
-    });
-    return res.json(post);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
-
-router.get("/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  try {
-    const post = await prisma.post.findUnique({
-      where: {
-        id,
-      },
-    });
-    return res.json(post);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
-
-router.delete("/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  try {
-    const post = await prisma.post.delete({
-      where: {
-        id,
-      },
-    });
-    return res.json(post);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
+router.get("/", PostController.index);
+router.get("/:id", PostController.show);
+router.post("/", PostController.create);
+router.patch("/:id", PostController.update);
+router.delete("/:id", PostController.delete);
 
 module.exports = router;
